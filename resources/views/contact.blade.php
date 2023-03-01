1c943da8b08987a1ed8bf1f6b3a155fec79e4876
Zarhoul <zarhoul21@gmail.com>
2023-03-01 18:43:55 +0100

    Add button Return and some changes in style

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Contact</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
         <style>
             body {
                 display: flex;
@@ -13,20 +14,23 @@
                 background-color: #e1e5f2;
             }
             h1 {
-                font-size: 2.5rem;
+                font-size: 4rem;
                 font-weight: bold;
                 margin: 50px 0 20px;
                 text-align: center;
             }
             form {
-                max-width: 600px;
+                max-width: 800px;
+                max-height: 400px;
                 padding: 1rem;
                 border: 1px solid #ccc;
                 border-radius: 5px;
                 width: 100%;
+                height: 100%;
             }
             label {
                 display: block;
+                font-size: 2rem;
                 font-weight: bold;
                 margin: 1rem 0 0.5rem;
             }
@@ -40,6 +44,12 @@
                 border: 1px solid #ccc;
                 border-radius: 5px;
             }
+            input[type="text"],
+            input[type="email"],
+            textarea:valid {
+                border: 1px solid #003049;
+                border-radius: 5px;
+            }
             textarea {
                 resize: vertical;
                 min-height: 100px;
@@ -47,7 +57,9 @@
             button[type="submit"] {
                 background-color: #2d88ff;
                 color: white;
-                padding: 0.5rem 1rem;
+                padding: 0.75rem 1.25rem;
+                font-size: 1.5rem;
+                font-weight: bold;
                 border: none;
                 border-radius: 5px;
                 cursor: pointer;
@@ -59,11 +71,40 @@
                 cursor: pointer;
                 transition: all 0.3s ease-in-out;
             }
+            a {
+                background-color: #ef233c;
+                color: white;
+                padding: 1rem 1.5rem;
+                text-decoration: none;
+                font-weight: bold;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                margin-top: 1rem;
+                transition: all 0.3s ease-in-out;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: row;
+                gap: 5px;
+            }
+            a:hover {
+                background-color: #d90429;
+                cursor: pointer;
+                transition: all 0.3s ease-in-out;
+                text-decoration: none;
+                color: #fff;
+            }
+            i {
+                font-size: 1rem;
+                font-weight: bold;
+            }
     </style>
         </style>
     </head>
     <body>
     <h1>Contact-Us!</h1>
+
     <form method="POST" action="/contact">
     @csrf
         <div>
@@ -80,5 +121,9 @@
         </div>
         <button type="submit">Send</button>
     </form>
+    <a href="{{ url('/') }}">
+        <i class="glyphicon glyphicon-remove"></i>
+        Return
+    </a>
     </body>
 </html>
--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Thank You</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
         <style>
             body {
                 display: flex;
@@ -14,14 +15,14 @@
             }
 
             h1 {
-                font-size: 2.5rem;
+                font-size: 4rem;
                 font-weight: bold;
                 margin: 50px 0 20px;
                 text-align: center;
             }
 
             p {
-                font-size: 1.25rem;
+                font-size: 2rem;
                 max-width: 500px;
                 text-align: center;
                 font-weight: bold;
@@ -39,20 +40,48 @@
                 align-items: center;
                 border: 1px solid #ccc;
                 border-radius: 5px;
-                max-width: 400px;
+                max-width: 500px;
                 width: 100%;
             }
 
             li {
                 margin-bottom: 0.5rem;
                 list-style: none;
-                font-size: 1rem;
+                font-size: 1.75rem;
             }
             label {
                 display: block;
-                font-size: 1.1rem;
+                font-size: 2rem;
                 font-weight: bold;
                 margin: 1rem 0 0.5rem;
+            }
+            a {
+                background-color: #ef233c;
+                color: white;
+                padding: 1rem 1.5rem;
+                text-decoration: none;
+                font-weight: bold;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                margin-top: 1rem;
+                transition: all 0.3s ease-in-out;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                flex-direction: row;
+                gap: 5px;
+            }
+            a:hover {
+                background-color: #d90429;
+                cursor: pointer;
+                transition: all 0.3s ease-in-out;
+                text-decoration: none;
+                color: #fff;
+            }
+            i {
+                font-size: 1rem;
+                font-weight: bold;
             }
         </style>
     </head>
@@ -73,6 +102,10 @@
                 {{ $data['message'] }}
             </li>
         </ul>
+        <a href="{{ url('/contact') }}">
+            <i class="glyphicon glyphicon-remove"></i>
+            Return
+        </a>
         <script>
             console.log(@json($data));
         </script>
